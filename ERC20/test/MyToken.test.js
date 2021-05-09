50a57ee3a7dd82346307cda0c04512601f069224
bart-antczak <antczak.bar@gmail.com>
2021-05-09 10:58:06 +0200

    ERC20: Added dotenv to project

--- ERC20/test/MyToken.test.js
+++ ERC20/test/MyToken.test.js
@@ -10,12 +10,13 @@ var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
 const expect = chai.expect;
+require('dotenv').config({path: '../.env'});
 
 contract("Token Test", function(accounts) {
     const [ initialHolder, recipient, anotherAccount ] = accounts;
 
     beforeEach(async () => {
-        this.myToken = await Token.new(1000);
+        this.myToken = await Token.new(process.env.INITIAL_TOKENS);
     });
 
     it("All tokens should be in my account", async () => {
