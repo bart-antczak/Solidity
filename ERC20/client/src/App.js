e4cf75c422b24aaa08084741765e57518c3f009c
bart-antczak <antczak.bar@gmail.com>
2021-05-10 08:55:18 +0200

    ERC20: Added HDWalletProvider and the Mnemonic to Truffle

--- ERC20/client/src/App.js
+++ ERC20/client/src/App.js
@@ -7,7 +7,7 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { loaded: false, kycAddress: "0x123" };
 
   componentDidMount = async () => {
     try {
@@ -47,37 +47,34 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
+  handleKycSubmit = async () => {
+    const {kycAddress} = this.state;
+    await this.kycContract.methods.setKycCompleted(kycAddress).send({from: this.accounts[0]});
+    alert("Account "+kycAddress+" is now whitelisted");
+  }
 
   render() {
     if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
-      </div>
+        <div className="App">
+          <h1>Capuccino Token for StarDucks</h1>
+
+          <h2>Enable your account</h2>
+          Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange} />
+          <button type="button" onClick={this.handleKycSubmit}>Add Address to Whitelist</button>
+        </div>
     );
   }
 }
